49fddb3288a43290cea1019acac079ea251e296f
WilliamPeterAldrik <148299717+WilliamPeterAldrik@users.noreply.github.com>
2026-04-12 21:06:49 +0700

    fixing queue problem

--- Script/key_listener.gd
+++ Script/key_listener.gd
@@ -16,10 +16,10 @@ var key_name: String = ""
 var falling_key_queue = []
 
 # Jika distance_from_pass lebih kecil dari threshold, beri score tersebut
-var perfect_press_threshold: float = 30
-var great_press_threshold: float = 50
-var good_press_threshold: float = 60
-var ok_press_threshold: float = 80
+var perfect_press_threshold: float = 50
+var great_press_threshold: float = 80
+var good_press_threshold: float = 100
+var ok_press_threshold: float = 150
 
 # Score untuk setiap threshold
 var perfect_press_score: float = 250
@@ -43,39 +43,52 @@ func _process(delta: float) -> void:
 	# Kalo button di pencet, key nya ilang
 	# TODO: Nentuin scoring berdasarkan jarak key jatoh n key input
 		if Input.is_action_just_pressed(key_name):
-			if falling_key_queue.size() > 0:
-				var key_to_pop = falling_key_queue.pop_front()
-				if is_instance_valid(key_to_pop):
-					var distance_from_pass = abs(key_to_pop.pass_threshold - key_to_pop.global_position.y)
-					
-					var press_score_text = ""
-					if distance_from_pass < perfect_press_threshold:
-						Signals.IncrementScore.emit(perfect_press_score)
-						press_score_text = "PERFECT"
-						Signals.IncrementCombo.emit()
-					elif distance_from_pass < great_press_threshold:
-						Signals.IncrementScore.emit(great_press_score)
-						press_score_text = "GREAT"
-						Signals.IncrementCombo.emit()
-					elif distance_from_pass < good_press_threshold:
-						Signals.IncrementScore.emit(good_press_score)
-						press_score_text = "GOOD"
-						Signals.IncrementCombo.emit()
-					elif distance_from_pass < ok_press_threshold:
-						Signals.IncrementScore.emit(ok_press_score)
-						press_score_text = "OK"
-						Signals.IncrementCombo.emit()
-					else:
-						press_score_text = "MISS"
-						Signals.ResetCombo.emit()
-						pass
-					
-					key_to_pop.queue_free()
-					
-					var st_inst = score_text.instantiate()
-					get_tree().get_root().call_deferred("add_child", st_inst)
-					st_inst.SetTextInfo(press_score_text)
-					st_inst.global_position = global_position
+			var key_to_pop = falling_key_queue.pop_front()
+			
+			var distance_from_pass = abs(key_to_pop.pass_threshold - key_to_pop.global_position.y)
+			
+			#$AnimationPlayer.stop()
+			#$AnimationPlayer.play("key_hit")
+			
+			var press_score_text: String = ""
+			if distance_from_pass < perfect_press_threshold:
+				Signals.IncrementScore.emit(perfect_press_score)
+				press_score_text = "PERFECT"
+				Signals.IncrementCombo.emit()
+			elif distance_from_pass < great_press_threshold:
+				Signals.IncrementScore.emit(great_press_score)
+				press_score_text = "GREAT"
+				Signals.IncrementCombo.emit()
+			elif distance_from_pass < good_press_threshold:
+				Signals.IncrementScore.emit(good_press_score)
+				press_score_text = "GOOD"
+				Signals.IncrementCombo.emit()
+			elif distance_from_pass < ok_press_threshold:
+				Signals.IncrementScore.emit(ok_press_score)
+				press_score_text = "OK"
+				Signals.IncrementCombo.emit()
+			else:
+				press_score_text = "MISS"
+				Signals.ResetCombo.emit()
+			
+			key_to_pop.queue_free()
+			
+			var st_inst = score_text.instantiate()
+			get_tree().get_root().call_deferred("add_child", st_inst)
+			st_inst.SetTextInfo(press_score_text)
+			st_inst.global_position = global_position + Vector2(0, -20)
+	
+	
+	
+
+func CreateFallingKey(button_name: String):
+	if button_name == key_name:
+		var fk_inst = falling_key.instantiate()
+		get_tree().get_root().call_deferred("add_child", fk_inst)
+		fk_inst.Setup(position.x, frame + 4)
+		
+		falling_key_queue.push_back(fk_inst)
+
 
 # Bikin Key nya jatoh
 func createFallingKey():
