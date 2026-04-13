12f1d10e0d7af4a46a1d0dceda26e16d976f2185
Elika N <153571556+blissels@users.noreply.github.com>
2026-04-13 09:52:53 +0700

    ui last minute :D

---
+++ Script/GameManager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# Variabel ini akan menyimpan nama lagu yang diklik pemain.
+# Kita beri nilai awal "DAHLAH" supaya kalau dites langsung main, gamenya nggak error.
+var selected_song: String = "DAHLAH"
--- Script/characterSect.gd
+++ Script/characterSect.gd
@@ -1,11 +1,19 @@
 extends Control
 
 signal back_pressed
+signal next_pressed # Sinyal baru untuk maju ke Song Selection
 
 func _ready():
-	# Sesuaikan path ini dengan posisi tombol Back kamu
-	var btn_back = $Background/MarginContainer/VBoxMain/TopBar/BtnBack
-	btn_back.pressed.connect(_on_btn_back_pressed)
+	# Tombol Back (Kiri Atas)
+	var btn_back = $Background/MarginContainer/VBoxMain/TopBar/BackButton
+	btn_back.pressed.connect(_on_back_button_pressed)
+	
+	# Tombol Pick Song (Bawah) - Sesuaikan path ini dengan namamu di Scene Tree
+	var btn_next = $Background/MarginContainer/VBoxMain/TopBar2/NextButton 
+	btn_next.pressed.connect(_on_next_button_pressed)
 
-func _on_btn_back_pressed():
-	back_pressed.emit()
+func _on_back_button_pressed():
+	pass # Replace with function body.
+
+func _on_next_button_pressed():
+	pass # Replace with function body.
